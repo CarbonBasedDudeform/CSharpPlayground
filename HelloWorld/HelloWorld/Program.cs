28e93c9bb5ae29712846c42efed51cb7787ae406
dom <contact@dominicmusgrave.co.uk>
2015-07-11 18:46:12 +0100

    checked keyword

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World! from Linux");
+
+			int overflowTest = 0;
+
+			Console.WriteLine ("value is : " + overflowTest);
+
+			overflowTest = int.MaxValue;
+
+			Console.WriteLine ("Value is : " + overflowTest);
+
+			int res = checked(overflowTest + 1);
+
+			Console.WriteLine ("Value is : " + overflowTest);
+
+		}
+	}
+}
